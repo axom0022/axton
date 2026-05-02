8a1757769760709e38e44b4d3a93ae7d02889bda
Axom <adamowaissi22@gmail.com>
2026-05-02 18:37:19 +0300

    Create register.c

---
+++ src/libs/register.c
@@ -0,0 +1,19 @@
+#include "../core/axton.h"
+
+void registermathlib(environment *env);
+void registertensorlib(environment *env);
+void registerguilib(environment *env);
+void registerdblib(environment *env);
+void registerasynclib(environment *env);
+void registerpackagelib(environment *env);
+void registerweblib(environment *env);
+
+void registeralllibs(environment *env) {
+    registermathlib(env);
+    registertensorlib(env);
+    registerguilib(env);
+    registerdblib(env);
+    registerasynclib(env);
+    registerpackagelib(env);
+    registerweblib(env);
+}
