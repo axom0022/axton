cbec7a7cfb53509149fb2a68ad86c6ded41254d1
Axom <adamowaissi22@gmail.com>
2026-05-02 18:24:43 +0300

    Update exception.c

--- src/core/exception.c
+++ src/core/exception.c
@@ -1,19 +1,19 @@
 #include "axton.h"
 
-static jmp_buf exception_jump;
-static object *exception_value = NULL;
+static jmp_buf exceptionjump;
+static object *exceptionvalue = NULL;
 
-void throw_exception(char *msg) {
-    exception_value = make_string(msg);
-    longjmp(exception_jump, 1);
+void throwexception(char *msg) {
+    exceptionvalue = makestring(msg);
+    longjmp(exceptionjump, 1);
 }
 
-object *catch_exception(void) {
-    object *val = exception_value;
-    exception_value = NULL;
+object *catchexception(void) {
+    object *val = exceptionvalue;
+    exceptionvalue = NULL;
     return val;
 }
 
-void init_exceptions(environment *env) {
-    env_set(env, "exception", make_class("exception", env_new(NULL), NULL), 0);
+void initexceptions(environment *env) {
+    envset(env, "exception", makeclass("exception", envnew(NULL), NULL), 0);
 }
