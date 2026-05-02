9a037ccc19a8a7404d967acd43c94a91d45a5e54
Axom <adamowaissi22@gmail.com>
2026-05-02 18:14:43 +0300

    Update axton.h

--- src/core/axton.h
+++ src/core/axton.h
@@ -20,7 +20,7 @@
 #define mkdir(x,y) _mkdir(x)
 #define strcasecmp _stricmp
 #define snprintf _snprintf
-#define PATH_SEP '\\'
+#define pathsep '\\'
 #else
 #include <unistd.h>
 #include <dirent.h>
@@ -29,14 +29,14 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#define PATH_SEP '/'
+#define pathsep '/'
 #endif
 
 #ifdef __ANDROID__
 #include <android/log.h>
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "axton", __VA_ARGS__)
+#define logi(...) __android_log_print(ANDROID_LOG_INFO, "axton", __VA_ARGS__)
 #else
-#define LOGI(...) printf(__VA_ARGS__)
+#define logi(...) printf(__VA_ARGS__)
 #endif
 
 #ifdef __EMSCRIPTEN__
@@ -46,24 +46,24 @@
 #ifdef __APPLE__
 #include <TargetConditionals.h>
 #if TARGET_OS_IPHONE
-#define PLATFORM_IOS 1
+#define platformios 1
 #else
-#define PLATFORM_MACOS 1
+#define platformmacos 1
 #endif
 #endif
 
 typedef enum {
-    TOK_EOF, TOK_IDENT, TOK_NUMBER, TOK_STRING,
-    TOK_INDENT, TOK_DEDENT, TOK_NEWLINE,
-    TOK_LET, TOK_CONST, TOK_FN, TOK_IF, TOK_ELSE, TOK_ELIF,
-    TOK_FOR, TOK_IN, TOK_WHILE, TOK_BREAK, TOK_NEXT, TOK_RETURN,
-    TOK_NONE, TOK_TRUE, TOK_FALSE, TOK_AND, TOK_OR, TOK_NOT,
-    TOK_PLUS, TOK_MINUS, TOK_STAR, TOK_SLASH, TOK_PERCENT,
-    TOK_EQ, TOK_EQEQ, TOK_NE, TOK_LT, TOK_GT, TOK_LE, TOK_GE,
-    TOK_LPAREN, TOK_RPAREN, TOK_LBRACKET, TOK_RBRACKET,
-    TOK_LBRACE, TOK_RBRACE, TOK_COMMA, TOK_DOT, TOK_COLON,
-    TOK_TRY, TOK_CATCH, TOK_FINALLY, TOK_THROW, TOK_CLASS, TOK_IMPORT,
-    TOK_ASYNC, TOK_AWAIT, TOK_YIELD
+    tokeof, tokident, toknumber, tokstring,
+    tokindent, tokdedent, toknewline,
+    toklet, tokconst, tokfn, tokif, tokelse, tokelif,
+    tokfor, tokin, tokwhile, tokbreak, toknext, tokreturn,
+    toknone, toktrue, tokfalse, tokand, tokor, toknot,
+    tokplus, tokminus, tokstar, tokslash, tokpercent,
+    tokeq, tokeqeq, tokne, toklt, tokgt, tokle, tokge,
+    toklparen, tokrparen, toklbracket, tokrbracket,
+    toklbrace, tokrbrace, tokcomma, tokdot, tokcolon,
+    tokcry, tokcatch, tokfinally, tokthrow, tokclass, tokimport,
+    tokasync, tokawait, tokyield
 } toktype;
 
 typedef struct token {
@@ -178,79 +178,80 @@ typedef struct {
     int (*writefile)(const char*, const char*);
     void *(*loadlib)(const char*);
     void *(*getproc)(void*, const char*);
-} platform_api;
+} platformapi;
 
 extern environment *globalenv;
 extern frame *currentframe;
 extern int tcount;
-extern platform_api platform;
+extern platformapi platform;
 
-void gc_init(void);
-void gc_add_root(object *obj);
-void gc_run(void);
-object *gc_alloc(int size);
+void gcinit(void);
+void gcaddroot(object *obj);
+void gcrun(void);
+object *gcalloc(int size);
 
-object *make_int(long v);
-object *make_float(double v);
-object *make_string(char *v);
-object *make_bool(int v);
-object *make_none(void);
-object *make_list(void);
-object *make_dict(void);
-object *make_func(char **params, int pcount, stmt **body, int bcount, environment *closure, char *name);
-object *make_builtin(void *(*fn)(object**, int, environment*));
-object *make_class(char *name, environment *attrs, object *super);
-object *make_instance(object *klass);
-object *make_range(long start, long stop, long step);
-object *make_module(char *name, void *handle);
-object *make_native(void *handle, void *data);
+object *makeint(long v);
+object *makefloat(double v);
+object *makestring(char *v);
+object *makebool(int v);
+object *makenone(void);
+object *makelist(void);
+object *makedict(void);
+object *makefunc(char **params, int pcount, stmt **body, int bcount, environment *closure, char *name);
+object *makebuiltin(void *(*fn)(object**, int, environment*));
+object *makeclass(char *name, environment *attrs, object *super);
+object *makeinstance(object *klass);
+object *makerange(long start, long stop, long step);
+object *makemodule(char *name, void *handle);
+object *makenative(void *handle, void *data);
 
-void list_append(object *list, object *item);
-object *list_get(object *list, long idx);
-void dict_set(object *dict, object *key, object *val);
-object *dict_get(object *dict, object *key);
-int dict_has(object *dict, object *key);
+void listappend(object *list, object *item);
+object *listget(object *list, long idx);
+void listset(object *list, long idx, object *item);
+void dictset(object *dict, object *key, object *val);
+object *dictget(object *dict, object *key);
+int dicthas(object *dict, object *key);
 
-environment *env_new(environment *parent);
-void env_set(environment *env, char *name, object *val, int cnst);
-object *env_get(environment *env, char *name);
+environment *envnew(environment *parent);
+void envset(environment *env, char *name, object *val, int cnst);
+object *envget(environment *env, char *name);
 
-int is_truthy(object *v);
-int values_equal(object *a, object *b);
-object *add_values(object *a, object *b);
-object *sub_values(object *a, object *b);
-object *mul_values(object *a, object *b);
-object *div_values(object *a, object *b);
-int less_than(object *a, object *b);
-int greater_than(object *a, object *b);
+int istruthy(object *v);
+int valuesequal(object *a, object *b);
+object *addvalues(object *a, object *b);
+object *subvalues(object *a, object *b);
+object *mulvalues(object *a, object *b);
+object *divvalues(object *a, object *b);
+int lessthan(object *a, object *b);
+int greaterthan(object *a, object *b);
 
-void throw_exception(char *msg);
-object *catch_exception(void);
-void init_exceptions(environment *env);
+void throwexception(char *msg);
+object *catchexception(void);
+void initexceptions(environment *env);
 
 token *tokenize(char *input);
-stmt *parse_tokens(token *tokens, int count);
-object *eval_program(stmt *program, environment *env);
-object *call_function(object *fn, object **args, int argc, environment *env);
-void register_builtins(environment *env);
-void register_stdlib(environment *env);
+stmt *parsetokens(token *tokens, int count);
+object *evalprogram(stmt *program, environment *env);
+object *callfunc(object *fn, object **args, int argc, environment *env);
+void registerbuiltins(environment *env);
+void registerstdlib(environment *env);
 
-void repl_start(void);
-void debugger_break(int line);
-void debugger_inspect(char *var);
-void profiler_start(void);
-void profiler_stop(void);
-char *format_code(char *source);
-char **lint_code(char *source, int *count);
-char *generate_docs(char *source);
+void replstart(void);
+void breakpoint(int line);
+void inspectvar(char *var);
+void profilestart(void);
+void profilestop(void);
+char *formatcode(char *source);
+char **lintcode(char *source, int *count);
+char *generatedocs(char *source);
 
-void platform_init(void);
-void platform_log(const char *msg);
-void platform_sleep(double seconds);
-double platform_time(void);
-char *platform_read_file(const char *path);
-int platform_write_file(const char *path, const char *content);
-void *platform_load_library(const char *path);
-void *platform_get_proc(void *lib, const char *name);
+void platforminit(void);
+void platformlog(const char *msg);
+void platformsleep(double seconds);
+double platformtime(void);
+char *platformreadfile(const char *path);
+int platformwritefile(const char *path, const char *content);
+void *platformloadlib(const char *path);
+void *platformgetproc(void *lib, const char *name);
 
 #endif
