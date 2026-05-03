30cf63971722891863d056241fb875e4d28fbf53
Axom <adamowaissi22@gmail.com>
2026-05-03 17:32:31 +0300

    Update async.c

--- src/libs/async.c
+++ src/libs/async.c
@@ -1,12 +1,40 @@
 #include "../core/axton.h"
 
-object *makecoroutine(object *func) {
-    object *obj = gcalloc(sizeof(object));
-    obj->type = 21;
-    obj->coroutine.func = func;
-    obj->coroutine.state = 0;
-    return obj;
+#ifdef __linux__
+#include <sys/epoll.h>
+#include <sys/eventfd.h>
+
+static int epollfd = -1;
+static object *taskqueue = NULL;
+
+void asyncloopadd(object *task) {
+    if (!taskqueue) {
+        taskqueue = makelist();
+    }
+    listappend(taskqueue, task);
 }
+
+void asynclooprun(void) {
+    if (epollfd < 0) {
+        epollfd = epoll_create1(0);
+    }
+    struct epoll_event events[64];
+    while (taskqueue && taskqueue->list.count > 0) {
+        int nfds = epoll_wait(epollfd, events, 64, 100);
+        for (int i = 0; i < taskqueue->list.count; i++) {
+            object *task = taskqueue->list.items[i];
+            if (task->type == 21 && task->coroutine.state == 0) {
+                callfunc(task->coroutine.func, NULL, 0, globalenv);
+                task->coroutine.state = 1;
+            }
+        }
+        taskqueue->list.count = 0;
+    }
+}
+#else
+void asyncloopadd(object *task) {}
+void asynclooprun(void) {}
+#endif
 
 object *asynccreate(object *func) {
     return makecoroutine(func);
@@ -14,7 +42,7 @@ object *asynccreate(object *func) {
 
 object *asyncawait(object *coro) {
     if (coro->type != 21) throwexception("not a coroutine");
-    callfunc(coro->coroutine.func, NULL, 0, globalenv);
+    asyncloopadd(coro);
     return makenone();
 }
 
@@ -29,6 +57,7 @@ object *builtinawait(object **args, int argc, environment *env) {
 }
 
 object *builtinrunloop(object **args, int argc, environment *env) {
+    asynclooprun();
     return makenone();
 }
 
