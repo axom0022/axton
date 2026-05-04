c4c63eb58d4cc52edbeea5a469523d91a1077591
Axom <adamowaissi22@gmail.com>
2026-05-04 17:58:22 +0300

    Update exception.c

--- src/core/exception.c
+++ src/core/exception.c
@@ -1,19 +1,24 @@
 #include "axton.h"
 
 static jmp_buf exceptionjump;
-static object *exceptionvalue = NULL;
+static object *exvalue = NULL;
 
 void throwexception(char *msg) {
-    exceptionvalue = makestring(msg);
+    exvalue = makestring(msg);
     longjmp(exceptionjump, 1);
 }
 
 object *catchexception(void) {
-    object *val = exceptionvalue;
-    exceptionvalue = NULL;
+    object *val = exvalue;
+    exvalue = NULL;
     return val;
 }
 
 void initexceptions(environment *env) {
     envset(env, "exception", makeclass("exception", envnew(NULL), NULL), 0);
+    envset(env, "typeerror", makeclass("typeerror", envnew(NULL), envget(env, "exception")), 0);
+    envset(env, "valueerror", makeclass("valueerror", envnew(NULL), envget(env, "exception")), 0);
+    envset(env, "keyerror", makeclass("keyerror", envnew(NULL), envget(env, "exception")), 0);
+    envset(env, "indexerror", makeclass("indexerror", envnew(NULL), envget(env, "exception")), 0);
+    envset(env, "attributeerror", makeclass("attributeerror", envnew(NULL), envget(env, "exception")), 0);
 }
