2705043f3f45d0720683887deb196b707d33de86
Axom <adamowaissi22@gmail.com>
2026-05-04 17:23:01 +0300

    Update graphics.h

--- src/libs/graphics.h
+++ src/libs/graphics.h
@@ -3,183 +3,99 @@
 
 #include "../core/axton.h"
 
-typedef struct matrix4 {
-    float m[16];
-} matrix4;
+typedef struct app app;
+typedef struct window window;
+typedef struct widget widget;
+typedef struct canvas canvas;
+typedef struct engine3d engine3d;
 
-typedef struct vector3 {
-    float x,y,z;
-} vector3;
+app* createapp(void);
+void runapp(app* a);
+void stopapp(app* a);
 
-typedef struct vector2 {
-    float x,y;
-} vector2;
+window* newwindow(app* a, char* title, int w, int h);
+widget* newbutton(window* w, char* text, int x, int y, int wd, int ht, object* onclick);
+widget* newlabel(window* w, char* text, int x, int y);
+widget* newtextbox(window* w, int x, int y, int wd, object* onchange);
+widget* newcheckbox(window* w, char* text, int x, int y, object* oncheck);
+widget* newradio(window* w, char* text, int x, int y, object* onselect);
+widget* newlistbox(window* w, int x, int y, int wd, int ht, object* onselect);
+widget* newdropdown(window* w, object* items, int x, int y, int wd, object* onselect);
+widget* newslider(window* w, int x, int y, int wd, int min, int max, object* onchange);
+widget* newprogress(window* w, int x, int y, int wd, int ht);
+widget* newimage(window* w, char* path, int x, int y, int wd, int ht);
+widget* newwebview(window* w, char* url, int x, int y, int wd, int ht);
+widget* newtable(window* w, object* cols, object* rows, int x, int y, int wd, int ht);
+widget* newchart(window* w, object* data, int x, int y, int wd, int ht);
+widget* newcanvas(window* w, int wd, int ht, object* ondraw);
+widget* newmenubar(window* w);
+widget* newmenu(widget* bar, char* name);
+widget* newmenuitem(widget* menu, char* name, object* onclick);
+widget* newtabs(window* w, int x, int y, int wd, int ht);
+widget* newtabpage(widget* tabs, char* title);
+widget* newpanel(window* w, int x, int y, int wd, int ht);
+widget* newspinner(window* w, int x, int y, int wd, object* onchange);
+widget* newdatepicker(window* w, int x, int y, int wd, object* onchange);
+widget* newcolorpicker(window* w, int x, int y, object* onchange);
+widget* newfilepicker(window* w, int x, int y, int wd, object* onselect);
+widget* newtoolbar(window* w);
+widget* newtoolbutton(widget* bar, char* icon, object* onclick);
+widget* newstatusbar(window* w);
+widget* newsplitter(window* w, int x, int y, int wd, int ht, int vertical);
+widget* newtree(window* w, object* nodes, int x, int y, int wd, int ht, object* onselect);
+widget* newhtmlview(window* w, char* html, int x, int y, int wd, int ht);
+widget* newmarkdown(window* w, char* text, int x, int y, int wd, int ht);
+widget* newvideo(window* w, char* path, int x, int y, int wd, int ht);
+widget* newaudio(window* w, char* path, int x, int y, int wd, int ht);
+widget* newmap(window* w, double lat, double lon, int zoom, int x, int y, int wd, int ht);
+widget* newpdfviewer(window* w, char* path, int x, int y, int wd, int ht);
+widget* newcodeeditor(window* w, char* code, int x, int y, int wd, int ht, object* onchange);
+widget* newterminal(window* w, int x, int y, int wd, int ht);
+widget* newnotebook(window* w, int x, int y, int wd, int ht);
+widget* newdebugger(window* w, int x, int y, int wd, int ht);
+widget* newlogger(window* w, int x, int y, int wd, int ht);
+widget* newtimer(window* w, int ms, object* ontimeout);
 
-typedef struct color {
-    float r,g,b,a;
-} color;
+void settext(widget* w, char* text);
+char* gettext(widget* w);
+void setvalue(widget* w, double val);
+double getvalue(widget* w);
+void appenditem(widget* w, char* item);
+void clearitems(widget* w);
+void show(widget* w);
+void hide(widget* w);
+void enable(widget* w);
+void disable(widget* w);
+void focus(widget* w);
+void destroy(widget* w);
 
-typedef struct mesh {
-    float *vertices;
-    int *indices;
-    int vcount;
-    int icount;
-    unsigned int vao;
-    unsigned int vbo;
-    unsigned int ebo;
-} mesh;
+void msgbox(char* title, char* msg);
+char* opendialog(char* title, char* filter);
+char* savedialog(char* title, char* filter);
+void alert(char* msg);
+int confirm(char* msg);
+void notify(char* title, char* msg, int ms);
 
-typedef struct texture {
-    unsigned int id;
-    int width;
-    int height;
-} texture;
-
-typedef struct material {
-    color diffuse;
-    color specular;
-    float shininess;
-    texture *diffusetex;
-} material;
-
-typedef struct model {
-    mesh *meshes;
-    material *materials;
-    int meshcount;
-    matrix4 transform;
-} model;
-
-typedef struct camera {
-    vector3 pos;
-    vector3 target;
-    vector3 up;
-    float fov;
-    float aspect;
-    float near;
-    float far;
-    matrix4 view;
-    matrix4 proj;
-} camera;
-
-typedef struct light {
-    vector3 pos;
-    color diffuse;
-    color specular;
-} light;
-
-typedef struct renderer {
-    unsigned int program;
-    camera *cam;
-    light *lights;
-    int lightcount;
-} renderer;
-
-object *graphicsinit(int width, int height, char *title);
-object *graphicsclear(color c);
-object *graphicsbegin(void);
-object *graphicsend(void);
-object *graphicsloadmesh(char *path);
-object *graphicsloadtexture(char *path);
-object *graphicsdrawmodel(object *model, matrix4 transform);
-object *graphicscamera(vector3 pos, vector3 target);
-object *graphicssetlight(vector3 pos, color diff, color spec);
-object *graphicsrun(void);
-object *graphicsclose(void);
-
-// widgets
-object *widgetcreatewindow(object *parent, int x, int y, int w, int h, char *title);
-object *widgetcreatebutton(object *parent, int x, int y, int w, int h, char *text, object *onclick);
-object *widgetcreatelabel(object *parent, int x, int y, char *text);
-object *widgetcreatetextbox(object *parent, int x, int y, int w, char *text, object *onchange);
-object *widgetcreatecheckbox(object *parent, int x, int y, char *text, object *oncheck);
-object *widgetcreateradiobutton(object *parent, int x, int y, char *text, object *onselect);
-object *widgetcreatecombobox(object *parent, int x, int y, int w, object *items, object *onselect);
-object *widgetcreatelistbox(object *parent, int x, int y, int w, int h, object *items, object *onselect);
-object *widgetcreatetable(object *parent, int x, int y, int w, int h, int rows, int cols, object *ondata);
-object *widgetcreatetree(object *parent, int x, int y, int w, int h, object *onnode);
-object *widgetcreatetabcontrol(object *parent, int x, int y, int w, int h, object *tabs);
-object *widgetcreateprogressbar(object *parent, int x, int y, int w, int h, int min, int max, int value);
-object *widgetcreateslider(object *parent, int x, int y, int w, int h, int min, int max, int value, object *onchange);
-object *widgetcreatespinbox(object *parent, int x, int y, int w, int min, int max, int value, object *onchange);
-object *widgetcreatedatepicker(object *parent, int x, int y, int w, object *ondate);
-object *widgetcreatecolorpicker(object *parent, int x, int y, int w, color col, object *oncolor);
-object *widgetcreateimage(object *parent, int x, int y, int w, int h, char *path);
-object *widgetcreatecanvas(object *parent, int x, int y, int w, int h, object *ondraw);
-object *widgetcreatewebview(object *parent, int x, int y, int w, int h, char *url);
-object *widgetcreatechart(object *parent, int x, int y, int w, int h, object *data);
-object *widgetcreategauge(object *parent, int x, int y, int w, int h, int min, int max, int value);
-object *widgetcreatecalendar(object *parent, int x, int y, int w, int h, object *onchange);
-object *widgetcreatetoolbar(object *parent, int x, int y, int w, int h);
-object *widgetcreatemenu(object *parent, object *items);
-object *widgetcreatestatusbar(object *parent, char *text);
-object *widgetshownotification(char *title, char *message);
-object *widgetfiledialog(int open, char *title);
-object *widgetmessagebox(char *title, char *message, int buttons);
-object *widgetinputdialog(char *title, char *prompt);
-object *widgetcolordialog(color *col);
-object *widgetfontdialog(char **fontname, int *size);
-object *widgetprintdialog(void);
-object *widgetwizardcreate(object *parent, char *title, object *pages);
-object *widgetdockpanelcreate(object *parent, int x, int y, int w, int h);
-object *widgetsplitcontainer(object *parent, int x, int y, int w, int h, int orientation);
-object *widgetpanel(object *parent, int x, int y, int w, int h);
-object *widgetgroupbox(object *parent, int x, int y, int w, int h, char *title);
-object *widgetscrollview(object *parent, int x, int y, int w, int h);
-object *widgettreeview(object *parent, int x, int y, int w, int h);
-object *widgetlistviewadvanced(object *parent, int x, int y, int w, int h);
-object *widgetgridview(object *parent, int x, int y, int w, int h);
-object *widgettileview(object *parent, int x, int y, int w, int h);
-object *widgetcarousel(object *parent, int x, int y, int w, int h, object *pages);
-object *widgetaccordion(object *parent, int x, int y, int w, int h, object *panels);
-object *widgetratingbar(object *parent, int x, int y, int max);
-object *widgetswitch(object *parent, int x, int y, int checked, object *onchange);
-object *widgettogglebutton(object *parent, int x, int y, int w, int h, char *text, int toggled, object *ontoggle);
-object *widgetsegmentedcontrol(object *parent, int x, int y, int w, int h, object *segments, object *onselect);
-object *widgetstepper(object *parent, int x, int y, int w, int h, int min, int max, int step, object *onchange);
-object *widgetknob(object *parent, int x, int y, int size, int min, int max, int value, object *onchange);
-object *widgetterminal(object *parent, int x, int y, int w, int h, object *oncommand);
-object *widgetcodeeditor(object *parent, int x, int y, int w, int h, char *language, char *code, object *onchange);
-object *widgetmarkdownview(object *parent, int x, int y, int w, int h, char *markdown);
-object *widgetvideoplayer(object *parent, int x, int y, int w, int h, char *path);
-object *widgetaudioplayer(object *parent, char *path);
-object *widgetwebbrowser(object *parent, int x, int y, int w, int h, char *url);
-object *widgetmapview(object *parent, int x, int y, int w, int h, double lat, double lon);
-object *widget3dview(object *parent, int x, int y, int w, int h);
-object *widgetparticlesystem(object *parent, int x, int y, int w, int h);
-object *widgetanimation(object *target, char *property, int duration, object *values);
-object *widgettransition(object *from, object *to, int duration, object *oncomplete);
-object *widgetgesturerecognizer(object *target, char *type, object *onrecognize);
-object *widgetdragdrop(object *source, object *target, object *ondrop);
-object *widgetclipboard(char *text);
-object *widgetundoredo(object *manager, object *command);
-object *widgettheme(char *name, object *colors);
-object *widgetstylesheet(char *css);
-object *widgetlayoutgrid(object *container, int cols, int rows);
-object *widgetlayoutflex(object *container, char *direction);
-object *widgetlayoutabsolute(object *container);
-object *widgetdatabinding(object *source, char *sourceprop, object *target, char *targetprop);
-object *widgetvalidation(object *input, char *rule, char *errormsg);
-object *widgetcommandbinding(object *button, char *command, object *execute);
-object *widgetmultiwindowcreate(char *title, int w, int h);
-object *widgettrayicon(char *iconpath, object *onclick);
-object *widgetglobalshortcut(char *key, object *onpress);
-object *widgetpowermanagementprevent(void);
-object *widgetscreencapture(int x, int y, int w, int h);
-object *widgetocr(char *imagepath);
-object *widgetspeechrecognize(int duration);
-object *widgetspeechspeak(char *text);
-object *widgetcameracapture(int device, int width, int height);
-object *widgetmicrophonerecord(int seconds);
-object *widgetserialportopen(char *port, int baud);
-object *widgetserialportwrite(char *data);
-object *widgetserialportread(void);
-object *widgetbluetoothscan(void);
-object *widgetbluetoothconnect(char *addr);
-object *widgetwifiscan(void);
-object *widgetwifisend(char *data);
-object *widgetsystemnotification(char *title, char *body);
-object *widgetprintingprint(char *html);
-object *widgetpdfgenerate(char *html, char *path);
-object *widgetexportexcel(object *data, char *path);
+engine3d* newengine3d(int w, int h);
+void render3d(engine3d* e);
+void rotate3d(engine3d* e, float x, float y, float z);
+void translate3d(engine3d* e, float x, float y, float z);
+void scale3d(engine3d* e, float x, float y, float z);
+void loadmodel(engine3d* e, char* path);
+void loadtexture(engine3d* e, char* path);
+void addlight(engine3d* e, float x, float y, float z, float r, float g, float b);
+void setcamera(engine3d* e, float x, float y, float z, float tx, float ty, float tz);
+void setbackground(engine3d* e, float r, float g, float b);
+void wireframe(engine3d* e, int on);
+void setshader(engine3d* e, char* vert, char* frag);
+void billboard(engine3d* e, char* text, float x, float y, float z);
+void particles(engine3d* e, int count, float x, float y, float z);
+void animate3d(engine3d* e, float speed);
+void raycast(engine3d* e, int x, int y, object* callback);
+void enablecollision(engine3d* e, int on);
+void setgravity(engine3d* e, float force);
+void enablephysics(engine3d* e, int on);
+void setjoystick(engine3d* e, int idx, object* callback);
+void enablevr(engine3d* e, int on);
 
 #endif
