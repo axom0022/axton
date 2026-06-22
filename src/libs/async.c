f2cd7ad758e41cc385f735c149ce6862e6ac438f
Axom <adamowaissi22@gmail.com>
2026-06-22 11:14:44 +0300

    Update async.c

--- src/libs/async.c
+++ src/libs/async.c
@@ -1,70 +1,36 @@
 #include "../core/axton.h"
-
-#ifdef __linux__
 #include <sys/epoll.h>
-#include <sys/eventfd.h>
+#include <unistd.h>
 
 static int epollfd = -1;
-static object *taskqueue = NULL;
 
-void asyncloopadd(object *task) {
-    if (!taskqueue) {
-        taskqueue = makelist();
-    }
-    listappend(taskqueue, task);
+object *asyncloop(object **a, int c, void *e) {
+    if (epollfd == -1) epollfd = epoll_create1(0);
+    return makeint(epollfd);
 }
 
-void asynclooprun(void) {
-    if (epollfd < 0) {
-        epollfd = epoll_create1(0);
-    }
-    struct epoll_event events[64];
-    while (taskqueue && taskqueue->list.count > 0) {
-        int nfds = epoll_wait(epollfd, events, 64, 100);
-        for (int i = 0; i < taskqueue->list.count; i++) {
-            object *task = taskqueue->list.items[i];
-            if (task->type == 21 && task->coroutine.state == 0) {
-                callfunc(task->coroutine.func, NULL, 0, globalenv);
-                task->coroutine.state = 1;
-            }
-        }
-        taskqueue->list.count = 0;
-    }
-}
-#else
-void asyncloopadd(object *task) {}
-void asynclooprun(void) {}
-#endif
-
-object *asynccreate(object *func) {
-    return makecoroutine(func);
-}
-
-object *asyncawait(object *coro) {
-    if (coro->type != 21) throwexception("not a coroutine");
-    asyncloopadd(coro);
+object *asyncsleep(object **a, int c, void *e) {
+    if (c < 1) throwexception("sleep needs seconds");
+    double sec = a[0]->fval;
+    platformsleep(sec);
     return makenone();
 }
 
-object *builtinasync(object **args, int argc, environment *env) {
-    if (argc < 1) throwexception("async needs function");
-    return asynccreate(args[0]);
+object *asyncaddtask(object **a, int c, void *e) {
+    if (c < 1) throwexception("addtask needs coroutine");
+    return makenone();
 }
 
-object *builtinawait(object **args, int argc, environment *env) {
-    if (argc < 1) throwexception("await needs coroutine");
-    return asyncawait(args[0]);
-}
-
-object *builtinrunloop(object **args, int argc, environment *env) {
-    asynclooprun();
+object *asyncawait(object **a, int c, void *e) {
+    if (c < 1) throwexception("await needs task");
     return makenone();
 }
 
 void registerasynclib(environment *env) {
-    object *asyncmod = makemodule("async", NULL);
-    envset(asyncmod->module.exports, "create", makebuiltin(builtinasync), 0);
-    envset(asyncmod->module.exports, "await", makebuiltin(builtinawait), 0);
-    envset(asyncmod->module.exports, "run", makebuiltin(builtinrunloop), 0);
-    envset(env, "async", asyncmod, 0);
+    object *mod = makemodule("async", NULL);
+    envset(mod->module.exports, "loop", makebuiltin(asyncloop), 0);
+    envset(mod->module.exports, "sleep", makebuiltin(asyncsleep), 0);
+    envset(mod->module.exports, "addtask", makebuiltin(asyncaddtask), 0);
+    envset(mod->module.exports, "await", makebuiltin(asyncawait), 0);
+    envset(env, "async", mod, 0);
 }
