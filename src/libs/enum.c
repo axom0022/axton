b7c1627373266b9c2437451c4aa88eed6dacdcbc
Axom <adamowaissi22@gmail.com>
2026-06-22 11:07:04 +0300

    Update enum.c

--- src/libs/enum.c
+++ src/libs/enum.c
@@ -1,18 +1,11 @@
 #include "../core/axton.h"
 
-object *enumclass(object **args, int argc, environment *env) {
-    if (argc < 2) throwexception("enum needs name and values");
-    char *name = args[0]->sval;
-    object *values = args[1];
-    if (values->type != 6 && values->type != 5) throwexception("enum needs dict or list");
+object *enumcreate(object **a, int c, void *e) {
+    if (c < 2) throwexception("enum needs name and values");
+    char *name = a[0]->sval;
+    object *values = a[1];
     object *cls = makeclass(name, envnew(NULL), NULL);
-    if (values->type == 6) {
-        for (int i = 0; i < values->dict.count; i++) {
-            char *key = values->dict.keyvals[i]->sval;
-            object *val = values->dict.vals[i];
-            envset(cls->klass.attrs, key, val, 0);
-        }
-    } else {
+    if (values->type == 5) {
         for (int i = 0; i < values->list.count; i++) {
             char *key = values->list.items[i]->sval;
             envset(cls->klass.attrs, key, makeint(i), 0);
@@ -22,5 +15,7 @@ object *enumclass(object **args, int argc, environment *env) {
 }
 
 void registerenumlib(environment *env) {
-    envset(env, "Enum", makebuiltin(enumclass), 0);
+    object *mod = makemodule("enum", NULL);
+    envset(mod->module.exports, "create", makebuiltin(enumcreate), 0);
+    envset(env, "enum", mod, 0);
 }
