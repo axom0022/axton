7a51e50db96e052a34f0c7c0f97a9d579979d493
Axom <adamowaissi22@gmail.com>
2026-06-22 11:06:37 +0300

    Update dataclass.c

--- src/libs/dataclass.c
+++ src/libs/dataclass.c
@@ -1,36 +1,16 @@
 #include "../core/axton.h"
 
-object *dataclass(object **args, int argc, environment *env) {
-    if (argc < 2) throwexception("dataclass needs name and fields");
-    char *name = args[0]->sval;
-    object *fields = args[1];
-    if (fields->type != 5) throwexception("fields must be list");
+object *dataclasscreate(object **a, int c, void *e) {
+    if (c < 2) throwexception("dataclass needs name and fields");
+    char *name = a[0]->sval;
+    object *fields = a[1];
     object *cls = makeclass(name, envnew(NULL), NULL);
-    envset(cls->klass.attrs, "__dataclass_fields__", fields, 0);
-    char *init_params = malloc(1024);
-    char *init_body = malloc(2048);
-    strcpy(init_params, "(");
-    strcpy(init_body, "");
-    for (int i = 0; i < fields->list.count; i++) {
-        char *fname = fields->list.items[i]->sval;
-        if (i > 0) strcat(init_params, ", ");
-        strcat(init_params, fname);
-        char line[256];
-        snprintf(line, sizeof(line), "    let this.%s = %s\n", fname, fname);
-        strcat(init_body, line);
-    }
-    strcat(init_params, ")");
-    char *init_code = malloc(4096);
-    snprintf(init_code, 4096, "let __init__ = fn%s {\n%s}", init_params, init_body);
-    token *toks = tokenize(init_code);
-    stmt *prog = parsetokens(toks, tcount);
-    evalprogram(prog, cls->klass.attrs);
-    free(init_params);
-    free(init_body);
-    free(init_code);
+    envset(cls->klass.attrs, "__fields__", fields, 0);
     return cls;
 }
 
 void registerdataclasslib(environment *env) {
-    envset(env, "dataclass", makebuiltin(dataclass), 0);
+    object *mod = makemodule("dataclass", NULL);
+    envset(mod->module.exports, "create", makebuiltin(dataclasscreate), 0);
+    envset(env, "dataclass", mod, 0);
 }
