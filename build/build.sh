b1be726311df42feeb95e02f69fbf5ce1a0bc753
Axom <adamowaissi22@gmail.com>
2026-06-26 12:46:03 +0300

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,120 +1,100 @@
 #!/bin/bash
 mkdir -p bin
 
+find_sources() {
+    find src -name "*.c" 2>/dev/null | tr '\n' ' '
+}
+
+detect_lib() {
+    local libname=$1
+    if command -v pkg-config >/dev/null 2>&1 && pkg-config --exists $libname 2>/dev/null; then
+        pkg-config --libs $libname
+    else
+        echo "-l$libname"
+    fi
+}
+
+get_platform_file() {
+    local platform=$1
+    case "$platform" in
+        linux) echo "src/platform/linux.c" ;;
+        linuxarm) echo "src/platform/linux.c" ;;
+        windows) echo "src/platform/windows.c" ;;
+        windows32) echo "src/platform/windows.c" ;;
+        macos) echo "src/platform/macos.c" ;;
+        macosarm) echo "src/platform/macos.c" ;;
+        android) echo "src/platform/android.c" ;;
+        ios) echo "src/platform/ios.c" ;;
+        web) echo "src/platform/webasm.c" ;;
+        freebsd) echo "src/platform/freebsd.c" ;;
+        *) echo "" ;;
+    esac
+}
+
+buildplatform() {
+    local name=$1
+    local cc=$2
+    local cflags=$3
+    local libs=$4
+    local platformfile=$5
+    local output=$6
+
+    echo "building for $name"
+    SOURCES=$(find_sources)
+    if [ -z "$SOURCES" ]; then
+        echo "error: no source files found"
+        exit 1
+    fi
+    $cc -o $output $SOURCES $platformfile $cflags $libs
+    echo "done: $output"
+}
+
+LIBS="-lm -ldl -lpthread -lsqlite3 -lz -lssl -lcrypto -lcurl -lffi"
+LIBS="$LIBS $(detect_lib glfw3) $(detect_lib BulletDynamics) $(detect_lib BulletCollision) $(detect_lib LinearMath) $(detect_lib openal)"
+
 buildlinux() {
-    echo "building for linux"
-    gcc -o bin/axton_linux \
-        src/core/*.c \
-        src/platform/linux.c \
-        src/tools/*.c \
-        src/libs/*.c \
-        -lm -ldl -lpthread -lsqlite3 -lz -lX11 -lssl -lcrypto -lcurl -lpcap -lbluetooth -ltesseract -ljpeg -lpng -lffi -lglfw -lGL -lGLU
-    echo "done"
+    buildplatform "linux" "gcc" "" "$LIBS -lX11 -lpcap -lbluetooth -ltesseract -ljpeg -lpng -lGL -lGLU" "src/platform/linux.c" "bin/axton_linux"
 }
 
 buildlinuxarm() {
-    echo "building for linux arm64"
-    aarch64-linux-gnu-gcc -o bin/axton_linux_arm64 \
-        src/core/*.c \
-        src/platform/linux.c \
-        src/tools/*.c \
-        src/libs/*.c \
-        -lm -ldl -lpthread -lsqlite3 -lz -static -lcurl -lpcap -lbluetooth -ltesseract -ljpeg -lpng -lffi
-    echo "done"
+    buildplatform "linux arm64" "aarch64-linux-gnu-gcc" "-static" "$LIBS -lpcap -lbluetooth -ltesseract -ljpeg -lpng" "src/platform/linux.c" "bin/axton_linux_arm64"
 }
 
 buildwindows() {
-    echo "building for windows"
-    x86_64-w64-mingw32-gcc -o bin/axton.exe \
-        src/core/*.c \
-        src/platform/windows.c \
-        src/tools/*.c \
-        src/libs/*.c \
-        -lm -lws2_32 -lsqlite3 -lz -lssl -lcrypto -lcurl -lffi
-    echo "done"
+    buildplatform "windows" "x86_64-w64-mingw32-gcc" "" "$LIBS -lws2_32" "src/platform/windows.c" "bin/axton.exe"
 }
 
 buildwindows32() {
-    echo "building for windows 32bit"
-    i686-w64-mingw32-gcc -o bin/axton32.exe \
-        src/core/*.c \
-        src/platform/windows.c \
-        src/tools/*.c \
-        src/libs/*.c \
-        -lm -lws2_32 -lsqlite3 -lz -lssl -lcrypto -lcurl -lffi
-    echo "done"
+    buildplatform "windows 32bit" "i686-w64-mingw32-gcc" "" "$LIBS -lws2_32" "src/platform/windows.c" "bin/axton32.exe"
 }
 
 buildmacos() {
-    echo "building for macos intel"
-    gcc -o bin/axton_macos_intel \
-        src/core/*.c \
-        src/platform/macos.c \
-        src/tools/*.c \
-        src/libs/*.c \
-        -lm -ldl -lpthread -lsqlite3 -lz -lcurl -lpcap -ltesseract -ljpeg -lpng -lffi -framework OpenGL -framework Cocoa
-    echo "done"
+    buildplatform "macos intel" "gcc" "" "$LIBS -lpcap -ltesseract -ljpeg -lpng -framework OpenGL -framework Cocoa" "src/platform/macos.c" "bin/axton_macos_intel"
 }
 
 buildmacosarm() {
-    echo "building for macos arm64"
-    gcc -arch arm64 -o bin/axton_macos_arm \
-        src/core/*.c \
-        src/platform/macos.c \
-        src/tools/*.c \
-        src/libs/*.c \
-        -lm -ldl -lpthread -lsqlite3 -lz -lcurl -lpcap -ltesseract -ljpeg -lpng -lffi -framework OpenGL -framework Cocoa
-    echo "done"
+    buildplatform "macos arm64" "gcc" "-arch arm64" "$LIBS -lpcap -ltesseract -ljpeg -lpng -framework OpenGL -framework Cocoa" "src/platform/macos.c" "bin/axton_macos_arm"
 }
 
 buildandroid() {
-    echo "building for android"
     if [ -z "$ANDROID_NDK" ]; then
         echo "ANDROID_NDK not set"
         exit 1
     fi
     local compiler="$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android21-clang"
-    $compiler -o bin/axton_android \
-        src/core/*.c \
-        src/platform/android.c \
-        src/tools/*.c \
-        src/libs/*.c \
-        -lm -static -lcurl -lssl -lcrypto -lsqlite3
-    echo "done"
+    buildplatform "android" "$compiler" "-static" "$LIBS" "src/platform/android.c" "bin/axton_android"
 }
 
 buildios() {
-    echo "building for ios"
-    xcrun -sdk iphoneos clang -arch arm64 \
-        -o bin/axton_ios \
-        src/core/*.c \
-        src/platform/ios.c \
-        src/tools/*.c \
-        src/libs/*.c \
-        -lm -lcurl -lssl -lcrypto -lsqlite3
-    echo "done"
+    buildplatform "ios" "xcrun -sdk iphoneos clang" "-arch arm64" "$LIBS" "src/platform/ios.c" "bin/axton_ios"
 }
 
 buildweb() {
-    echo "building for web"
-    emcc -o bin/axton.html \
-        src/core/*.c \
-        src/platform/webasm.c \
-        src/tools/*.c \
-        src/libs/*.c \
-        -s WASM=1 -s EXPORTED_FUNCTIONS='["_main"]' -s USE_GLFW=3 -s USE_OPENGL=1
-    echo "done"
+    buildplatform "web" "emcc" "" "-s WASM=1 -s EXPORTED_FUNCTIONS='[\"_main\"]' -s USE_GLFW=3 -s USE_OPENGL=1 $LIBS" "src/platform/webasm.c" "bin/axton.html"
 }
 
 buildfreebsd() {
-    echo "building for freebsd"
-    clang -o bin/axton_freebsd \
-        src/core/*.c \
-        src/platform/freebsd.c \
-        src/tools/*.c \
-        src/libs/*.c \
-        -lm -lpthread -lsqlite3 -lz -lcurl -lpcap -lbluetooth -ltesseract -ljpeg -lpng -lffi
-    echo "done"
+    buildplatform "freebsd" "clang" "" "$LIBS -lpcap -lbluetooth -ltesseract -ljpeg -lpng" "src/platform/freebsd.c" "bin/axton_freebsd"
 }
 
 buildall() {
