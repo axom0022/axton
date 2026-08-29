3d8fe63e66d1a54d990d93f70643e9f78107aa52
Axom <adamowaissi22@gmail.com>
2026-08-29 03:04:42 +0300

    Update exception.c

--- src/core/exception.c
+++ src/core/exception.c
@@ -1,10 +1,30 @@
 #include "axton.h"
+#include <stdarg.h>
 
 static jmp_buf exceptionjump;
 static object *exvalue = NULL;
+static char exfile[256];
+static int exline = 0;
 
 void throwexception(char *msg) {
-    exvalue = makestring(msg);
+    char fullmsg[512];
+    if (currentframe && currentframe->file) {
+        snprintf(fullmsg, sizeof(fullmsg), "%s at %s:%d", msg, currentframe->file, currentframe->line);
+    } else {
+        snprintf(fullmsg, sizeof(fullmsg), "%s", msg);
+    }
+    exvalue = makestring(fullmsg);
+    longjmp(exceptionjump, 1);
+}
+
+void throwexceptiontype(char *type, char *msg) {
+    char full[512];
+    if (currentframe && currentframe->file) {
+        snprintf(full, sizeof(full), "%s: %s at %s:%d", type, msg, currentframe->file, currentframe->line);
+    } else {
+        snprintf(full, sizeof(full), "%s: %s", type, msg);
+    }
+    exvalue = makestring(full);
     longjmp(exceptionjump, 1);
 }
 
