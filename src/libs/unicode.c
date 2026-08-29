d6242350757f9722df18d98f2f591b8721e07f0c
Axom <adamowaissi22@gmail.com>
2026-08-29 03:11:09 +0300

    Update unicode.c

--- src/libs/unicode.c
+++ src/libs/unicode.c
@@ -2,20 +2,33 @@
 #include <wchar.h>
 #include <locale.h>
 
-object *unicodewidth(object **a, int c, void *e) {
-    if (c < 1) throwexception("width needs char");
-    wchar_t wc;
-    mbtowc(&wc, a[0]->sval, strlen(a[0]->sval));
-    return makeint(wcwidth(wc));
+object *unicodechar(object **args, int argc, void *env) {
+    if (argc < 1) throwexception("char needs utf8 string");
+    char *s = args[0]->sval;
+    int cp = platformunicodechar(s);
+    if (cp < 0) throwexception("invalid utf8");
+    return makeunicode(cp, s);
 }
 
-object *unicodenormalize(object **a, int c, void *e) {
-    if (c < 2) throwexception("normalize needs form and string");
-    return makestring(a[1]->sval);
+object *unicodewidth(object **args, int argc, void *env) {
+    if (argc < 1) throwexception("width needs unicode");
+    object *uc = args[0];
+    wchar_t wc = uc->unicode.codepoint;
+    int w = wcwidth(wc);
+    if (w < 0) w = 1;
+    return makeint(w);
+}
+
+object *unicodenormalize(object **args, int argc, void *env) {
+    if (argc < 2) throwexception("normalize needs form string");
+    char *form = args[0]->sval;
+    char *str = args[1]->sval;
+    return makestring(str);
 }
 
 void registerunicodelib(environment *env) {
     object *mod = makemodule("unicode", NULL);
+    envset(mod->module.exports, "char", makebuiltin(unicodechar), 0);
     envset(mod->module.exports, "width", makebuiltin(unicodewidth), 0);
     envset(mod->module.exports, "normalize", makebuiltin(unicodenormalize), 0);
     envset(env, "unicode", mod, 0);
