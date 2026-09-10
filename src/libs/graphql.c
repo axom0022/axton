3dbc72bd65648e97af743a3db0a8024c7b2df1ed
Axom <adamowaissi22@gmail.com>
2026-09-10 13:20:04 +0300

    Update graphql.c

--- src/libs/graphql.c
+++ src/libs/graphql.c
@@ -1,18 +1,42 @@
 #include "../core/axton.h"
 
-object *graphqlquery(object **a, int c, void *e) {
-    if (c < 2) throwexception("query needs endpoint and query");
-    return makedict();
+object *graphqlcreate(object **args, int argc, void *env) {
+    void *schema = platformgraphqlcreate();
+    if (!schema) throwexception("graphql create failed");
+    return makegraphql(schema);
 }
 
-object *graphqlmutate(object **a, int c, void *e) {
-    if (c < 2) throwexception("mutate needs endpoint and query");
-    return makedict();
+object *graphqladdquery(object **args, int argc, void *env) {
+    if (argc < 3) throwexception("addquery needs name fn");
+    object *gql = args[0];
+    char *name = args[1]->sval;
+    object *fn = args[2];
+    platformgraphqladdquery(gql->graphql.schema, name, fn);
+    return makenone();
 }
 
-void registergraphqllib(environment *env) {
+object *graphqladdmutation(object **args, int argc, void *env) {
+    if (argc < 3) throwexception("addmutation needs name fn");
+    object *gql = args[0];
+    char *name = args[1]->sval;
+    object *fn = args[2];
+    platformgraphqladdmutation(gql->graphql.schema, name, fn);
+    return makenone();
+}
+
+object *graphqlserve(object **args, int argc, void *env) {
+    if (argc < 2) throwexception("serve needs port");
+    object *gql = args[0];
+    int port = args[1]->ival;
+    void *server = platformgraphqlserve(gql->graphql.schema, port);
+    return makenative(server, NULL);
+}
+
+void registergraphqlmodule(environment *env) {
     object *mod = makemodule("graphql", NULL);
-    envset(mod->module.exports, "query", makebuiltin(graphqlquery), 0);
-    envset(mod->module.exports, "mutate", makebuiltin(graphqlmutate), 0);
+    envset(mod->module.exports, "create", makebuiltin(graphqlcreate), 0);
+    envset(mod->module.exports, "addquery", makebuiltin(graphqladdquery), 0);
+    envset(mod->module.exports, "addmutation", makebuiltin(graphqladdmutation), 0);
+    envset(mod->module.exports, "serve", makebuiltin(graphqlserve), 0);
     envset(env, "graphql", mod, 0);
 }
