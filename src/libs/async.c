4d2dec7b2ed0be145e6f08e280176f1be445aaa4
Axom <adamowaissi22@gmail.com>
2026-09-10 13:17:03 +0300

    Update async.c

--- src/libs/async.c
+++ src/libs/async.c
@@ -1,36 +1,30 @@
 #include "../core/axton.h"
-#include <sys/epoll.h>
-#include <unistd.h>
 
-static int epollfd = -1;
-
-object *asyncloop(object **a, int c, void *e) {
-    if (epollfd == -1) epollfd = epoll_create1(0);
-    return makeint(epollfd);
+object *asynccreate(object **args, int argc, void *env) {
+    void *loop = platformasynccreate();
+    if (!loop) throwexception("async create failed");
+    return makeasync(loop);
 }
 
-object *asyncsleep(object **a, int c, void *e) {
-    if (c < 1) throwexception("sleep needs seconds");
-    double sec = a[0]->fval;
-    platformsleep(sec);
+object *asyncadd(object **args, int argc, void *env) {
+    if (argc < 2) throwexception("add needs task");
+    object *async = args[0];
+    object *task = args[1];
+    platformasyncadd(async->async.loop, task);
     return makenone();
 }
 
-object *asyncaddtask(object **a, int c, void *e) {
-    if (c < 1) throwexception("addtask needs coroutine");
-    return makenone();
+object *asyncrun(object **args, int argc, void *env) {
+    if (argc < 1) throwexception("run needs async");
+    object *async = args[0];
+    int res = platformasyncrun(async->async.loop);
+    return makebool(res);
 }
 
-object *asyncawait(object **a, int c, void *e) {
-    if (c < 1) throwexception("await needs task");
-    return makenone();
-}
-
-void registerasynclib(environment *env) {
+void registerasyncmodule(environment *env) {
     object *mod = makemodule("async", NULL);
-    envset(mod->module.exports, "loop", makebuiltin(asyncloop), 0);
-    envset(mod->module.exports, "sleep", makebuiltin(asyncsleep), 0);
-    envset(mod->module.exports, "addtask", makebuiltin(asyncaddtask), 0);
-    envset(mod->module.exports, "await", makebuiltin(asyncawait), 0);
+    envset(mod->module.exports, "create", makebuiltin(asynccreate), 0);
+    envset(mod->module.exports, "add", makebuiltin(asyncadd), 0);
+    envset(mod->module.exports, "run", makebuiltin(asyncrun), 0);
     envset(env, "async", mod, 0);
 }
